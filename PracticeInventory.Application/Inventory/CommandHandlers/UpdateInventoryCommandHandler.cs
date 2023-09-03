ac36afb7cf79d8cb220ff20a140c9f72533a2ec9
Cristian Reyes Catchuela <cristiancatchuela15@gmail.com>
2023-09-03 20:52:14 +0800

    update

--- PracticeInventory.Application/Inventory/CommandHandlers/UpdateInventoryCommandHandler.cs
+++ PracticeInventory.Application/Inventory/CommandHandlers/UpdateInventoryCommandHandler.cs
@@ -16,15 +16,18 @@ public class UpdateInventoryCommandHandler : IRequestHandler<UpdateInventoryComm
     public async Task<Result<Guid>> Handle(UpdateInventoryCommand request, CancellationToken cancellationToken)
     {
         var findInventory = await _unitOfWork.InventoryRepository.GetByIdAsync(request.InventoryId);
+        if (findInventory is null)
+        {
+            return Result<Guid>.Failure("InventoryId is not exist.");
+        }
+
         findInventory.ItemId = request.ItemId;
         findInventory.Cost = request.Cost;
         findInventory.SalePrice = request.SalePrice;
         findInventory.CurrentQuantity = request.CurrentQuantity;
         findInventory.CriticalQuantity = request.CriticalQuantity;
 
-        var result = await _unitOfWork.CompleteAsync() > 0;
-
-        if (!result) return Result<Guid>.Failure("Failed to update inventory.");
+        await _unitOfWork.CompleteAsync();
 
         return Result<Guid>.Success(findInventory.InventoryId);
     }
